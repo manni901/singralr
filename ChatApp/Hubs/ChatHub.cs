03859921f0d40a34de1099411db51b604dea051f
manni901 <prashant24.juit@gmail.com>
2014-08-01 13:18:53 +0530

    initial commit

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace ChatApp
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public void Sand(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addChatMessageToPage(name, message);
+        }
+
+        public void Modesend(string newmode)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.changeLanguageMode(newmode);
+        }
+    }
+}
